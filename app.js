3fd5431ee498cef6dbc90f4871b1b2bcb9df50ed
soumdgir <ken5.2668.asashi3@gmail.com>
2026-07-10 21:56:11 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ const uploads = document.getElementById('up');
     }
 
     console.log("P2P紐付け完了:", p2pbox);
+    setupReceivedData({ on: () => {} }, p2pbox);
 
 const peer = new Peer({
         host: '0.peerjs.com',
